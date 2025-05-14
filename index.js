d53d577f82045474e3e20329bbfea563910a5e13
lulublumi <125117659+lulublumi@users.noreply.github.com>
2025-05-14 16:11:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
+const benutzerdaten = {
+  "testgmbh": [
+    {
+      username: "demo",
+      password: "1234",
+      kundennummer: "TG-1001",
+      ansprechpartner: "Sabine Müller"
+    },
+    {
+      username: "max",
+      password: "passwort",
+      kundennummer: "TG-1002",
+      ansprechpartner: "Felix Bauer"
+    }
+  ]
+};
+
 app.use(cors());
 app.use(express.json());
 
